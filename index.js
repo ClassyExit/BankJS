c0940825ccc8048ca6deb94037d62d06f64eee0f
Alex <alexgenriech10@gmail.com>
2022-04-24 21:32:21 -0700

    setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
