e84c3de2af0bc67845a15a4a069b0e3347b25f46
Alex <alexgenriech10@gmail.com>
2022-04-25 20:45:51 -0700

    Redo

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ["lkasld235j"], // contents of keys does not matter
+  })
+);
 
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
 
 app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
